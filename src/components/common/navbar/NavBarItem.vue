b993f5acb9f39735f3229d31aee4ee61b374ead3
XGD <1119363048@qq.com>
2021-04-13 18:37:36 +0800

    first commit

---
+++ src/components/common/navbar/NavBarItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tab-bar-item">
+    <div>
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+    color: white;
+  }
+</style>
